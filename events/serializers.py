8b9a29acbb0db864d6e1cae123b8cbbf3c37604c
Oleksandr Barabanov <oleksandr.barabanov.py@gmail.com>
2025-07-02 16:37:30 +0300

    Add EventRegistrationSerializer to the events/serializers.py

--- events/serializers.py
+++ events/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
-from events.models import Event
+from events.models import Event, EventRegistration
 
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = "__all__"
+
+
+class EventRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EventRegistration
+        fields = ["id", "user", "event", "registration_date"]
+        read_only_fields = ["registration_date"]
