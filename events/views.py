61a89ed4831b9aee23bd61894ce7bf38ab5263ad
Oleksandr Barabanov <oleksandr.barabanov.py@gmail.com>
2025-07-02 16:36:26 +0300

    Add EventRegistrationViewSet to the events/views.py

--- events/views.py
+++ events/views.py
@@ -1,7 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
-from events.models import Event
-from events.serializers import EventSerializer
+from events.models import Event, EventRegistration
+from events.serializers import EventSerializer, EventRegistrationSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
@@ -9,3 +9,12 @@ class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class EventRegistrationViewSet(viewsets.ModelViewSet):
+    queryset = EventRegistration.objects.all()
+    serializer_class = EventRegistrationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
