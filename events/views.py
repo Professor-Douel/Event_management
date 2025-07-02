ab3bc7128b76fa82448448e969ecab74dd775eed
Oleksandr Barabanov <oleksandr.barabanov.py@gmail.com>
2025-07-02 16:20:22 +0300

    Add EventViewSet to the events/views.py

--- events/views.py
+++ events/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from events.models import Event
+from events.serializers import EventSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
