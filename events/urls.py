8481ca99db41b42af6e46c5ec25bd4e7b7c037e5
Oleksandr Barabanov <oleksandr.barabanov.py@gmail.com>
2025-07-02 16:53:44 +0300

    Add 2 endpoints('events', 'registrations') to the created file  events/urls.py

---
+++ events/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import EventViewSet, EventRegistrationViewSet
+
+
+router = DefaultRouter()
+
+router.register("events", EventViewSet)
+
+router.register("registrations", EventRegistrationViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
