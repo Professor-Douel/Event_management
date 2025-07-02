784a1267e108beb6985eaf600e712ec655e4e791
Oleksandr Barabanov <oleksandr.barabanov.py@gmail.com>
2025-07-02 15:25:52 +0300

    Create model Event in events/models.py

--- events/models.py
+++ events/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
