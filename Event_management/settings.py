110cfc5d7337dfce53d2dc65103f8d0324f6e9a8
Oleksandr Barabanov <oleksandr.barabanov.py@gmail.com>
2025-07-03 11:23:38 +0300

    Add settings for the mailhog to the file settings.py

--- Event_management/settings.py
+++ Event_management/settings.py
@@ -143,3 +143,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = 'noreply@example.com'
