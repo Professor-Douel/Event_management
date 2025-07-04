531ef7487d569b94e5ca11d202d0436333602e77
Oleksandr Barabanov <oleksandr.barabanov.py@gmail.com>
2025-07-04 20:19:04 +0300

    Update settings.py

--- Event_management/settings.py
+++ Event_management/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 import rest_framework.authentication
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wv%-38ljtbszrvsr%*7!m38)ef-)b1b#6v3*si+!(&bs8w524$"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
